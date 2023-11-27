7a6f3faf54920ea0ae75311daf1741316e0389d6
biswajit-prog1990 <bisu.92.dutta@gmail.com>
2023-11-27 12:59:13 +0530

    Commit to LargestElement.java

--- com/arrays/problems/easy/LargestElement.java
+++ com/arrays/problems/easy/LargestElement.java
@@ -9,10 +9,30 @@ import java.util.Arrays;
 public class LargestElement {
 
     private static int findLargestElementBruteForce(int[] arr, int n) {
-        Arrays.sort(arr);
-        return arr[n-1];
+        Arrays.sort(arr); // O(n log n) SC -> O(1)
+        return arr[n - 1];
     }
 
+    private static int findLargestElementOptimized(int[] arr, int n) {
+        int largest = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > largest)
+                largest = arr[i];
+        }
+        return largest;
+    }
 
+    public static void main(String[] args) {
+        int[] arr = new int[]{5, 6, 19, 22, 1, 0, 16, 7, 6, 2, 22, 94, 56};
+        int n = arr.length;
+        long startTime1 = System.currentTimeMillis();
+        System.out.println("Largest Element : " + findLargestElementBruteForce(arr, n));
+        long endTime1 = System.currentTimeMillis();
+        System.out.println("Time Taken Brute Force : " + (endTime1 - startTime1) * 10);
+        long startTime = System.currentTimeMillis();
+        System.out.println("Largest Element : " + findLargestElementOptimized(arr, n));
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time Taken Optimized : " + (endTime - startTime) * 10);
+    }
 
 }
