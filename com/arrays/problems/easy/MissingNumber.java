f258d9e66840221a33233b056a776bd248f3ae31
biswajit-prog1990 <bisu.92.dutta@gmail.com>
2023-12-20 15:03:40 +0530

    Updated hashing method

--- com/arrays/problems/easy/MissingNumber.java
+++ com/arrays/problems/easy/MissingNumber.java
@@ -32,7 +32,7 @@ public class MissingNumber {
         int[] hash = new int[n + 1];
         for (int i = 0; i < n; i++)
             hash[arr[i]] = 1;
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             if (hash[i] == 0) return i;
         }
         return -1;
